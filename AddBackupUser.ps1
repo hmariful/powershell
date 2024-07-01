1cee9b35b7e674739e4a15e970f0bf934096ea57
Md Ariful Haque <74465094+hmariful@users.noreply.github.com>
2024-07-01 19:31:19 +0600

    Add files via upload

---
+++ AddBackupUser.ps1
@@ -0,0 +1,11 @@
+﻿$data=import-csv -Path "C:\Users\abu_r\Desktop\OutPut.csv"
+foreach($users in $data)
+    {
+        $user_name=$users.uName
+        $display_name=$users.dName
+        $org = Get-VBOOrganization
+        $user = Get-VBOOrganizationUser -Organization $org -UserName $user_name 
+        $backupItemUser=New-VBOBackupItem -User $user -Mailbox -ArchiveMailbox -OneDrive
+        $job = Get-VBOJob -Name "TestJob"
+        Add-VBOBackupItem -Job $job -BackupItem $backupItemUser
+    }
